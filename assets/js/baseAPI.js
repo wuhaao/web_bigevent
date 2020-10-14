bed32f0bf0d397542d483efa89abf8e35c3e8e21
wuhao <1104321162@qq.com>
2020-10-14 10:38:43 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -4,5 +4,22 @@
 $.ajaxPrefilter(function (options) {
     // 在发起真正的Ajax之前, 统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
-    //     console.log(options.url);
+
+    // 统一为有权限的接口设置 headers 请求头
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || '',
+        };
+    }
+
+    // 全局统一挂载complete回调函数
+    options.complete = function (res) {
+        // 在complete回调函数中,可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2.重新跳转到登录页
+            location.href = '/login.html';
+        }
+    };
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,19 +1,60 @@
 $(function () {
-    // 定义获取用户的基本信息的函数
-    function getUserInfo() {
-        $.ajax({
-            method: 'GET',
-            url: '/my/userinfo',
-            // herders是请求头配置对象
-            headers: {
-                Authorization: localStorage.getItem('token') || '',
-            },
-            success: function (res) {
-                console.log(res);
-            },
-        });
-    }
-
     // 调用getUserInfo()函数获取用户基本信息
     getUserInfo();
+
+    var layer = layui.layer;
+
+    // 点击退出按钮实现退出功能
+    $('#btnLogout').on('click', function () {
+        // 提示用户是否确认退出
+        layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+            //do something
+
+            // 1.清空本地存储中的token
+            localStorage.removeItem('token');
+            // 2.重新跳转到登录页
+            location.href = '/login.html';
+
+            // 关闭confirm询问框
+            layer.close(index);
+        });
+    });
 });
+
+// 定义获取用户的基本信息的函数
+function getUserInfo() {
+    $.ajax({
+        method: 'GET',
+        url: '/my/userinfo',
+        // herders是请求头配置对象
+        // headers: {
+        //     Authorization: localStorage.getItem('token') || '',
+        // },
+        success: function (res) {
+            if (res.status !== 0) {
+                return layui.layer.msg('获取用户信息失败!');
+            }
+            // 调用renderAvatar渲染用户头像
+            renderAvatar(res.data);
+        },
+    });
+}
+
+// 定义渲染用户头像的函数
+function renderAvatar(user) {
+    // 1.获取用户名称
+    var name = user.nickname || user.username;
+    // 2.设置欢迎的文本
+    $('#welcome').html('欢迎&nbsp;&nbsp' + name);
+    // 3.按需渲染用户头像
+    if (user.user_pic !== null) {
+        // 3.1渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide();
+    } else {
+        // 3.2渲染文本头像
+        $('.layui-nav-img').hide();
+        var first = name[0].toUpperCase();
+        $('.text-avatar').html(first).show();
+    }
+}
