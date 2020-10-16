94baa4c151680553529cb6699a2eb7b93e67f5b8
wuhao <1104321162@qq.com>
2020-10-16 17:59:50 +0800

    项目完成!

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -14,7 +14,6 @@ $.ajaxPrefilter(function (options) {
 
     // 全局统一挂载complete回调函数
     options.complete = function (res) {
-        console.log(res);
         // 在complete回调函数中,可以使用res.responseJSON拿到服务器响应回来的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
             // 1.清空本地存储中的token
